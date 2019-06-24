b912518673618eea1f99b55c259f3461ef0789d0
Andrew Brudnak <brudnak@icloud.com>
2019-06-23 23:59:13 -0700

    Setting up users

--- routes/users.js
+++ routes/users.js
@@ -19,13 +19,29 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400), json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+    } catch (err) {}
   }
 );
 
